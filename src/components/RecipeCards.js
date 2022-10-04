07ba76f073171288c88590fe405569a16565dc67
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2022-10-04 17:25:32 -0500

    Added RecipeCards.js

---
+++ src/components/RecipeCards.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import imageData from "../data/images";
+import { v4 as uuid } from 'uuid';
+
+function RecipeCards(){
+    const [cards, setCards] = useState([]);
+
+
+    useEffect(() => {
+        fetch("http://localhost:3001/cards")
+        .then((response) => response.json())
+        .then((data) => {
+          setCards(data);
+        });
+      }, []);
+    
+      return(
+        <div>
+            {cards.length > 0 ? cards.map((card) => (
+            <div key={uuid()} className="recipeCards">
+                <img src={imageData[card.title[0].image].link} alt={imageData[card.title[0].image].alt} className="recipeImage" />
+                <p className="recipeName">
+                    {card.title[0].recipeName}
+                </p>
+                {card.ingredients.map((data, index) => (
+                    <li key={index}>
+                        {data.ingredient} x {data.quantity} {data.measurement}
+                    </li>
+                ))}
+                <p>
+                    {card.comments[0].body}
+                </p>
+                <button>Remove recipe</button>
+            </div>
+            
+            )): null}
+        </div>
+
+      )
+}
+
+export default RecipeCards;
